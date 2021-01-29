65ae47e2226d2afacbc0eda0b67fb21457823459
Adam Toy <adam.toy@rancherfederal.com>
2021-01-29 10:07:30 -0500

    Line cleanup

--- modules/common/download.sh
+++ modules/common/download.sh
@@ -2,24 +2,19 @@
 set -e
 
 preflight_container_selinux_check () {
-    if [[ $1 == $2 ]]
-    then
+    if [[ $1 == $2 ]]; then
         return 0
     fi
     local IFS=.
     local i ver1=($1) ver2=($2)
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
         ver1[i]=0
     done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
+    for ((i=0; i<${#ver1[@]}; i++)); do
+        if [[ -z ${ver2[i]} ]]; then
             ver2[i]=0
         fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
+        if ((10#${ver1[i]} < 10#${ver2[i]})); then
             return 1
         fi
     done
