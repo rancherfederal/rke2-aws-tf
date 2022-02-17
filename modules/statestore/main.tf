8c4c8235ad3dbb2ec22f03f26a67af9ac8b66749
Robert Kernick <rkernick@oteemo.com>
2022-02-17 12:51:34 -0500

    Update for terraform aws module 4.x

--- modules/statestore/main.tf
+++ modules/statestore/main.tf
@@ -1,17 +1,23 @@
 resource "aws_s3_bucket" "bucket" {
   bucket        = lower("${var.name}-rke2")
-  acl           = "private"
   force_destroy = true
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
+  tags = merge({}, var.tags)
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "ssec" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
     }
   }
-
-  tags = merge({}, var.tags)
 }
 
 resource "aws_s3_bucket_object" "token" {
