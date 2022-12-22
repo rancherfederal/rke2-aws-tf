c423f7f1bd00c2549bdcabdcac3299cd4c82b588
Scott Miller <scott@millergeek.xyz>
2022-12-22 12:31:06 -0500

    use `versions.tf` rather than `providers.tf` for provider versions

--- modules/agent-nodepool/versions.tf
+++ modules/agent-nodepool/versions.tf
@@ -1,3 +1,15 @@
 terraform {
   required_version = ">= 0.13"
+
+  required_providers {
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.2.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
 }
--- versions.tf
+++ versions.tf
@@ -1,3 +1,18 @@
 terraform {
   required_version = ">= 0.13"
+
+  required_providers {
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.2.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+  }
 }
