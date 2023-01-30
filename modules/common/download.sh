edd1531f68142007bde60d09d9cfa23f9b930075
Adam Leiner <adam.leiner@rancherfederal.com>
2023-01-30 12:16:38 -0500

    cleanup

--- modules/common/download.sh
+++ modules/common/download.sh
@@ -109,6 +109,7 @@ do_download() {
     hostnamectl set-hostname "$(curl http://169.254.169.254/latest/meta-data/hostname)"
 
     INSTALL_RKE2_METHOD='tar' INSTALL_RKE2_TYPE="${type}" ./install.sh
+    
     install_awscli
     ;;
   amzn)
