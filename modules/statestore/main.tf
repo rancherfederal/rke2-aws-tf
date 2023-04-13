3dc0b76bae5884af7483b546200b62fc92b28ac4
Meredith McGlynn <meredith.mcglynn@braingu.com>
2023-04-13 12:40:10 -0400

    feat: adding in bucket ownership controls

--- modules/statestore/main.tf
+++ modules/statestore/main.tf
@@ -6,8 +6,16 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket_owner_control]
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_owner_control" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "ssec" {
