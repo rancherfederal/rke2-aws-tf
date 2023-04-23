2e19f29aa3f40598d613348db1b70b5a2cf80981
Rex Wang <rexwangcc@gmail.com>
2023-04-23 17:50:10 +0800

    Add aws_s3_bucket_ownership_controls to statestore to mimic the default behavior before 2023.04 of AWS S3.

--- modules/statestore/main.tf
+++ modules/statestore/main.tf
@@ -5,9 +5,25 @@ resource "aws_s3_bucket" "bucket" {
   tags = merge({}, var.tags)
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+
+  depends_on = [
+    aws_s3_bucket.bucket
+  ]
+}
+
 resource "aws_s3_bucket_acl" "acl" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket_ownership
+  ]
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "ssec" {
