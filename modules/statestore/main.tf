071e1cb51b592972f4652f6b51b0fa1a3e7153db
Rex Wang <rexwangcc@gmail.com>
2023-04-23 17:42:01 +0800

    Fix missing id property.

--- modules/statestore/main.tf
+++ modules/statestore/main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
-  bucket = aws_s3_bucket.bucket
+  bucket = aws_s3_bucket.bucket.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
